d1e316052862ee7bad9444af61b6449187258470
Alexander Chumakov <Alexander.Chumakov@harman.com>
2015-11-26 11:10:23 +0300

    Added build dependency on libcurl

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <curl/curl.h>
 
 int main(int argc, char **argv)
 {
+  CURLcode code = curl_global_init(CURL_GLOBAL_SSL);
   printf("Hello from main!\n");
+
+  // clean-up
+  curl_global_cleanup();
   return 0;
 }
